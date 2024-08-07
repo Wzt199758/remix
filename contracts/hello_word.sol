b123cf9e9137ddc79b70e9fdbd36acc58ec26363
Wzt199758 <656417056@qq.com>
2024-08-07 16:30:59 +0800

    init project

---
+++ contracts/hello_word.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+contract HellowHord {
+    string storeMsg;
+    function set(string memory message) public {
+        storeMsg = message;
+    }
+    function get() public view returns (string memory){
+        return storeMsg;
+    } 
+}
